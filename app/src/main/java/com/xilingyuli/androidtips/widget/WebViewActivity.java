3e0011bf14e4684e608d0e65d706df767e2f2bf8
xilingyuli <786979248@qq.com>
2017-05-14 22:50:17 +0800

    收藏网址功能

--- app/src/main/java/com/xilingyuli/androidtips/widget/WebViewActivity.java
+++ app/src/main/java/com/xilingyuli/androidtips/widget/WebViewActivity.java
@@ -1,17 +1,32 @@
 package com.xilingyuli.androidtips.widget;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
+import com.tencent.cos.COSClient;
+import com.tencent.cos.model.COSRequest;
+import com.tencent.cos.model.COSResult;
+import com.tencent.cos.model.PutObjectRequest;
+import com.tencent.cos.task.listener.ICmdTaskListener;
 import com.xilingyuli.androidtips.R;
+import com.xilingyuli.androidtips.model.CloudDataHelper;
+import com.xilingyuli.androidtips.model.CloudDataUtil;
+import com.xilingyuli.androidtips.utils.FileUtil;
+
+import java.io.File;
+import java.net.URLEncoder;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,6 +36,7 @@ public class WebViewActivity extends AppCompatActivity {
     @BindView(R.id.webview)
     WebView webView;
 
+    COSClient client;
     String url;
 
     @Override
@@ -83,8 +99,53 @@ public class WebViewActivity extends AppCompatActivity {
                 finish();
                 break;
             case R.id.favorite:
+                addFavorite();
+                break;
+            case R.id.open_in_browser:
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(webView.getUrl()));
+                startActivity(Intent.createChooser(intent, "请选择浏览器"));
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void addFavorite()
+    {
+        final View view = getLayoutInflater().inflate(R.layout.dialog_insert_link,null);
+        final TextInputEditText name = (TextInputEditText)view.findViewById(R.id.linkName);
+        final TextInputEditText url = (TextInputEditText)view.findViewById(R.id.linkUrl);
+        name.setText(webView.getTitle());
+        url.setText(webView.getUrl());
+        new AlertDialog.Builder(this)
+                .setTitle("Add Site")
+                .setView(view)
+                .setPositiveButton("确定", (dialogInterface, i) -> saveSite(name.getText() + "", url.getText() + ""))
+                .setNegativeButton("取消",null)
+                .show();
+    }
+
+    @SuppressWarnings("deprecation")
+    public void saveSite(String name, String url){
+        String fileName = URLEncoder.encode(name)+" "+URLEncoder.encode(url);
+        FileUtil.saveFile(fileName,"");
+        if(client==null)
+            client = CloudDataUtil.createCOSClient(this);
+        PutObjectRequest request = (PutObjectRequest) CloudDataHelper.createCOSRequest(
+                CloudDataHelper.ACTION_UPLOAD_FAVORITE,
+                new ICmdTaskListener() {
+                    @Override
+                    public void onSuccess(COSRequest cosRequest, COSResult cosResult) {
+                        runOnUiThread(()-> Toast.makeText(WebViewActivity.this,"已收藏",Toast.LENGTH_SHORT).show());
+                    }
+
+                    @Override
+                    public void onFailed(COSRequest cosRequest, COSResult cosResult) {
+                        runOnUiThread(()-> Toast.makeText(WebViewActivity.this,"收藏失败",Toast.LENGTH_SHORT).show());
+                    }
+                },
+                new File(FileUtil.ROOT_PATH+fileName));
+        client.putObject(request);
     }
 }
